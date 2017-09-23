c9110b62267b0eaffcf290f35057579e1ba4a746
SMuyesser <smuyesser@gmail.com>
2017-09-22 17:07:15 -0700

    fix

--- src/__tests__/formation.js
+++ src/__tests__/formation.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {shallow} from 'enzyme';
-
 import {Formations} from '../components/formation';
 
 it('renders without crashing', () => {
