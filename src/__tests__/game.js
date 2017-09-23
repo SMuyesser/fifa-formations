e906c504295e713e316be369e690c23fa67e68e3
SMuyesser <smuyesser@gmail.com>
2017-09-22 17:20:16 -0700

    removed game test

--- src/__tests__/game.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import axios from 'axios';
-import {shallow} from 'enzyme';
-import {Game} from '../components/game';
-
-it('renders without crashing', () => {
-	shallow(<Game />);
-});
